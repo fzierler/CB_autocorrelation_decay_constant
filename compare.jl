7090ea967119988e0f6eba349656f8355c835971
Fabian Zierler <fabian.zierler@a1.net>
2025-05-22 09:17:57 +0100

    plot Niccolò's data

--- compare.jl
+++ compare.jl
@@ -1,33 +1,29 @@
 using Pkg;Pkg.activate("./src/parse")
 using JSON
+using Plots
+using HDF5
 using Statistics
+gr(fontfamily="Computer Modern",legend=:topright,frame=:box,titlefontsize=11,legendfontsize=9,labelfontsize=12,left_margin=0Plots.mm)
 
 path      = "./external_data/smeared/" 
-ensembles = [
-    "Sp4b6.5nF2nAS3mF-0.71mAS-1.01T48L20",
-    "Sp4b6.5nF2nAS3mF-0.71mAS-1.01T64L20",
-    "Sp4b6.5nF2nAS3mF-0.71mAS-1.01T96L20",
-    "Sp4b6.5nF2nAS3mF-0.7mAS-1.01T64L20",
-    "Sp4b6.5nF2nAS3mF-0.72mAS-1.01T64L32",
-]
+ensembles = Dict(
+    "M1" => "Sp4b6.5nF2nAS3mF-0.71mAS-1.01T48L20",
+    "M2" => "Sp4b6.5nF2nAS3mF-0.71mAS-1.01T64L20",
+    "M3" => "Sp4b6.5nF2nAS3mF-0.71mAS-1.01T96L20",
+    "M4" => "Sp4b6.5nF2nAS3mF-0.7mAS-1.01T64L20",
+    "M5" => "Sp4b6.5nF2nAS3mF-0.72mAS-1.01T64L32",
+)
 
-function mass_and_decay_constant_unrenormalized(path,ens)
-    json_file_π = joinpath(path,ens,"meson_extraction_f_ps_samples.json")
-    json_file_ρ = joinpath(path,ens,"meson_extraction_f_v_samples.json")
-    data_π = JSON.parsefile(json_file_π)
-    data_ρ = JSON.parsefile(json_file_ρ)
-    fπ   = mean(data_π["f_ps_matrix_element_samples"]./sqrt.(data_π["f_ps_mass_samples"]))
-    Δfπ  = std( data_π["f_ps_matrix_element_samples"]./sqrt.(data_π["f_ps_mass_samples"]))
-    fρ   = mean(data_ρ["f_v_matrix_element_samples"]./sqrt.(data_ρ["f_v_mass_samples"]))
-    Δfρ  = std( data_ρ["f_v_matrix_element_samples"]./sqrt.(data_ρ["f_v_mass_samples"]))
-    mπ   = data_π["f_ps_mass_value"]
-    mρ   = data_ρ["f_v_mass_value"]
-    Δmπ  = std(data_π["f_ps_mass_samples"])
-    Δmρ  = std(data_ρ["f_v_mass_samples"])
-    return mπ, Δmπ, mρ, Δmρ, fπ, Δfπ, fρ, Δfρ
+function mass_and_decay_constant_unrenormalized(path,ens,rep,channel)
+    json_file = joinpath(path,ens,"meson_extraction_$(rep)_$(channel)_samples.json")
+    data = JSON.parsefile(json_file)
+    f    = mean(data["$(rep)_$(channel)_matrix_element_samples"]./sqrt.(data["$(rep)_$(channel)_mass_samples"]))
+    Δf   = std( data["$(rep)_$(channel)_matrix_element_samples"]./sqrt.(data["$(rep)_$(channel)_mass_samples"]))
+    m    = data["$(rep)_$(channel)_mass_value"]
+    Δm   = std(data["$(rep)_$(channel)_mass_samples"])
+    return f, Δf, m, Δm
 end
 
-using HDF5
 h5file = "data_assets/test.hdf5" 
 fid    = h5open(h5file)
 P      = [ mean(fid["M$(i)FUN"]["plaquette"][]) for i in 1:5 ]
@@ -39,3 +35,17 @@ ZA_FUN = ZA.(5/4,beta,P)
 ZV_FUN = ZV.(5/4,beta,P)
 ZA_AS  = ZA.(2  ,beta,P)
 ZV_AS  = ZV.(2  ,beta,P)
+
+plt = plot(xticks=(1:5,["M$i" for i in 1:5]))
+for i in 1:5
+    ens = "M$i"
+    fPS, ΔfPS = mass_and_decay_constant_unrenormalized(path,ensembles[ens],"f" ,"ps")[1:2]
+    fps, Δfps = mass_and_decay_constant_unrenormalized(path,ensembles[ens],"as","ps")[1:2]
+    fV,  ΔfV  = mass_and_decay_constant_unrenormalized(path,ensembles[ens],"f" ,"v" )[1:2]
+    fv,  Δfv  = mass_and_decay_constant_unrenormalized(path,ensembles[ens],"as","v" )[1:2]
+    scatter!(plt,[i],[fPS], yerr=[ΔfPS], label="", alpha=0.8, shape=:rect,      color=:green)
+    scatter!(plt,[i],[fps], yerr=[Δfps], label="", alpha=0.8, shape=:circ,      color=:blue)
+    scatter!(plt,[i],[fV],  yerr=[ΔfV],  label="", alpha=0.8, shape=:hexagon,   color=:pink)
+    scatter!(plt,[i],[fv],  yerr=[Δfv],  label="", alpha=0.8, shape=:utriangle, color=:orange)
+end
+plt
